6314f5281af538538b428a21d5ee8b2c28dc076e
Afiq Harith <afiqharith15@gmail.com>
2023-02-07 15:53:56 +0800

    update 3

--- lib/models/weather/weather.dart
+++ lib/models/weather/weather.dart
@@ -1,1 +1,51 @@
-class Weather {}
+import 'package:weather_now/models/weather_data_hourly.dart';
+
+import 'current.dart';
+import 'daily.dart';
+import 'hourly.dart';
+
+class Weather {
+  double? lat;
+  double? lon;
+  String? timezone;
+  int? timezoneOffset;
+  Current? current;
+  List<Hourly>? hourly;
+  List<Daily>? daily;
+
+  Weather({
+    this.lat,
+    this.lon,
+    this.timezone,
+    this.timezoneOffset,
+    this.current,
+    this.hourly,
+    this.daily,
+  });
+
+  factory Weather.fromJson(Map<String, dynamic> json) => Weather(
+        lat: (json['lat'] as num?)?.toDouble(),
+        lon: (json['lon'] as num?)?.toDouble(),
+        timezone: json['timezone'] as String?,
+        timezoneOffset: json['timezone_offset'] as int?,
+        current: json['current'] == null
+            ? null
+            : Current.fromJson(json['current'] as Map<String, dynamic>),
+        hourly: (json['daily'] as List<dynamic>?)
+            ?.map((e) => Hourly.fromJson(e as Map<String, dynamic>))
+            .toList(),
+        daily: (json['daily'] as List<dynamic>?)
+            ?.map((e) => Daily.fromJson(e as Map<String, dynamic>))
+            .toList(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'lat': lat,
+        'lon': lon,
+        'timezone': timezone,
+        'timezone_offset': timezoneOffset,
+        'current': current?.toJson(),
+        'hourly': hourly?.map((e) => e.toJson()).toList(),
+        'daily': daily?.map((e) => e.toJson()).toList(),
+      };
+}
